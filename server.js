8b3a3a88888529460f8774e434b458369b34b164
Eoin Ocathasaigh <G00417466@atu.ie>
2024-10-25 10:54:52 +0000

    Completed Exercise 5 - Returning JSON Data as a response - Creating our own api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 //Defining a port to execute on
 const port = 3000;
 
-//Routing
+//Routing - Listening for a certain request
 //Req - http request
 //Res - http response
 //Essentially we get a response in the form of text "Hello world"
@@ -21,16 +21,55 @@ app.get('/', (req, res) => {
 //It will get the url request and return us a message based on the name we enter with it
 //Eg. If I use ../hello/Eoin in my search bar - Hello Eoin is returned to me
 //Can be altered to add more parameters to it
+//:parameter is how we create a new parameter
+//const param = req.params.param is how we access it
 app.get('/hello/:name/:secondname', (req, res) => {
     const name = req.params.name;
     const secondname = req.params.secondname;
     res.send(`Hello ${name} ${secondname}`);
+    //Can also just include them in the send 
+    //- res.send(`Hello ` +req.params.name);
+});
+
+//Route to return a list of movies objects in JSON format
+//We're creating our own api
+app.get('/api/movies', (req, res) => {
+    //We essentially create our own array of movie items
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    //Returning a response to the user
+    //Note - status code in 200 means a successful response
+    res.status(200).json({ myMovies:movies });
 });
 
 //This is an error handling component
 //Like the name implies it acts as middleware to catch any server errors
 app.use((err, req, res, next) => {
     console.error(err.stack);
+    //Sets a status code of some sort
+    //This one is 500 - implies a server side error
+    //400 = client side error
     res.status(500).send('Something went wrong!');
 });
 
